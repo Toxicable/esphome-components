459e6570ce7d1e9b93080da790540850388f3fe6
Fabian Wiles <fabian.wiles@gmail.com>
2026-01-23 20:37:07 +0000

    wire in ldec

--- components/drv8243/output.py
+++ components/drv8243/output.py
@@ -1,12 +1,13 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import output
+from esphome.components import ledc, output
 from esphome.const import CONF_ID
 from esphome import pins
 
 from . import DRV8243Output
 
 CONF_OUT1 = "out1"
+CONF_LEDC = "ledc"
 CONF_NSLEEP_PIN = "nsleep_pin"
 CONF_NFAULT_PIN = "nfault_pin"
 CONF_OUT2_PIN = "out2_pin"
@@ -14,22 +15,34 @@ CONF_FLIP_POLARITY = "flip_polarity"
 CONF_MIN_LEVEL = "min_level"
 CONF_EXPONENT = "exponent"
 
-CONFIG_SCHEMA = output.FLOAT_OUTPUT_SCHEMA.extend(
-    {
-        cv.GenerateID(): cv.declare_id(DRV8243Output),
+def _validate_out1_config(config):
+    if CONF_OUT1 in config and CONF_LEDC in config:
+        raise cv.Invalid("Specify only one of out1 or ledc")
+    if CONF_OUT1 not in config and CONF_LEDC not in config:
+        raise cv.Invalid("Specify either out1 or ledc")
+    return config
 
-        cv.Required(CONF_OUT1): cv.use_id(output.FloatOutput),
 
-        cv.Required(CONF_NSLEEP_PIN): pins.gpio_output_pin_schema,
-        cv.Optional(CONF_NFAULT_PIN): pins.gpio_input_pin_schema,
+CONFIG_SCHEMA = cv.All(
+    output.FLOAT_OUTPUT_SCHEMA.extend(
+        {
+            cv.GenerateID(): cv.declare_id(DRV8243Output),
 
-        cv.Optional(CONF_OUT2_PIN): pins.gpio_output_pin_schema,
-        cv.Optional(CONF_FLIP_POLARITY, default=False): cv.boolean,
+            cv.Optional(CONF_OUT1): cv.use_id(output.FloatOutput),
+            cv.Optional(CONF_LEDC): ledc.CONFIG_SCHEMA,
 
-        cv.Optional(CONF_MIN_LEVEL, default=0.014): cv.percentage,
-        cv.Optional(CONF_EXPONENT, default=1.8): cv.float_range(min=0.1, max=5.0),
-    }
-).extend(cv.COMPONENT_SCHEMA)
+            cv.Required(CONF_NSLEEP_PIN): pins.gpio_output_pin_schema,
+            cv.Optional(CONF_NFAULT_PIN): pins.gpio_input_pin_schema,
+
+            cv.Optional(CONF_OUT2_PIN): pins.gpio_output_pin_schema,
+            cv.Optional(CONF_FLIP_POLARITY, default=False): cv.boolean,
+
+            cv.Optional(CONF_MIN_LEVEL, default=0.014): cv.percentage,
+            cv.Optional(CONF_EXPONENT, default=1.8): cv.float_range(min=0.1, max=5.0),
+        }
+    ).extend(cv.COMPONENT_SCHEMA),
+    _validate_out1_config,
+)
 
 
 async def to_code(config):
@@ -37,7 +50,12 @@ async def to_code(config):
     await cg.register_component(var, config)
     await output.register_output(var, config)
 
-    out1 = await cg.get_variable(config[CONF_OUT1])
+    if CONF_LEDC in config:
+        ledc_config = config[CONF_LEDC]
+        await ledc.to_code(ledc_config)
+        out1 = await cg.get_variable(ledc_config[CONF_ID])
+    else:
+        out1 = await cg.get_variable(config[CONF_OUT1])
     cg.add(var.set_out1_output(out1))
 
     nsleep = await cg.gpio_pin_expression(config[CONF_NSLEEP_PIN])
