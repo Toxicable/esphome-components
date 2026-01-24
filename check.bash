fbe31ad8842c2f6494243a8aca1ec553faaa7249
Fabian Wiles <fabian.wiles@gmail.com>
2026-01-24 23:51:07 +0000

    dev time comile issues

--- check.bash
+++ check.bash
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+filter_cc1_args() {
+  awk '
+    /internal \(cc1\) args are:/ {next}
+    /Generic fallback command is:/ {next}
+    {print}
+  '
+}
+export -f filter_cc1_args
+
+if [[ $# -gt 0 ]]; then
+  clangd --check="$1" 2>&1 | filter_cc1_args
+  exit 0
+fi
+
 rg --files --null \
   -g "*.c" \
   -g "*.cc" \
@@ -9,4 +23,4 @@ rg --files --null \
   -g "*.h" \
   -g "*.hh" \
   -g "*.hpp" \
-  | xargs -0 -n 1 clangd --check
+  | xargs -0 -n 1 -I{} bash -c 'clangd --check="$1" 2>&1 | filter_cc1_args' _ {}
