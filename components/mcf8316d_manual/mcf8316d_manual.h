00c943dc128a6f2a94383ae40015408222fa662d
Fabian Wiles <fabian.wiles@gmail.com>
2026-03-10 10:40:07 +0000

    slow first cycle

--- components/mcf8316d_manual/mcf8316d_manual.h
+++ components/mcf8316d_manual/mcf8316d_manual.h
@@ -228,8 +228,8 @@ class MCF8316DManualComponent : public PollingComponent, public i2c::I2CDevice {
   static constexpr uint32_t STARTUP_TUNE_MTR_STARTUP = 3u;  // Slow first cycle startup
   static constexpr uint32_t STARTUP_TUNE_ALIGN_OR_SLOW_CURRENT_ILIMIT = 6u;
   static constexpr uint32_t STARTUP_TUNE_OL_ILIMIT = 6u;
-  static constexpr uint32_t STARTUP_TUNE_OPN_CL_HANDOFF_THR = 0x0Fu;
-  static constexpr uint32_t STARTUP_TUNE_SLOW_FIRST_CYC_FREQ = 6u;
+  static constexpr uint32_t STARTUP_TUNE_OPN_CL_HANDOFF_THR = 0x08u;
+  static constexpr uint32_t STARTUP_TUNE_SLOW_FIRST_CYC_FREQ = 1u;
   static constexpr uint32_t STARTUP_TUNE_FIRST_CYCLE_FREQ_SEL = 1u;
   static constexpr uint32_t STARTUP_TUNE_MAX_SPEED = 0x2710u;  // 10000 -> 1666 Hz electrical
 
