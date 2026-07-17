1785d4a97804c2ed428150a789cfecd166776623
Fabian Wiles <fabianwiles@canva.com>
2026-07-17 10:22:36 +1000

    Refresh charger snapshots through the typed interface

--- components/bq25756/bq25756_interface.cpp
+++ components/bq25756/bq25756_interface.cpp
@@ -1,5 +1,7 @@
 #include "bq25756.h"
 
+#include "esphome/core/hal.h"
+
 namespace esphome {
 namespace bq25756 {
 
@@ -15,7 +17,25 @@ namespace bq25756 {
 }
 
 ::component_common::ChargerSnapshot BQ25756Component::snapshot() const {
-  return this->charger_snapshot_;
+  auto *self = const_cast<BQ25756Component *>(this);
+  ::bq25756_core::Status status{};
+  ::bq25756_core::Measurements measurements{};
+  ::bq25756_core::ControlStates controls{};
+  ::bq25756_core::AdcConfigurationState adc_state{};
+
+  if (!self->service_.read_status(status) ||
+      self->service_.read_measurements(
+          measurements, false, ::bq25756_core::REG2B_ADC_CONTINUOUS_15_BIT,
+          adc_state) != ::bq25756_core::MeasurementReadResult::OK ||
+      !self->service_.read_control_states(controls)) {
+    auto unavailable = self->charger_snapshot_;
+    unavailable.valid = false;
+    return unavailable;
+  }
+
+  self->charger_snapshot_ = ::bq25756_core::make_charger_snapshot(
+      status, measurements, controls, self->charger_snapshot_.sequence + 1U, millis());
+  return self->charger_snapshot_;
 }
 
 bool BQ25756Component::request_enabled(bool enabled) {
