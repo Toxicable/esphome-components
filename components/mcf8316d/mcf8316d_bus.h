ba88d2fd14fa266ad1d76de68fe490263f116cea
Fabian Wiles <fabianwiles@canva.com>
2026-07-19 19:34:07 +1000

    Fix MCF8316D typed register namespace

--- components/mcf8316d/mcf8316d_bus.h
+++ components/mcf8316d/mcf8316d_bus.h
@@ -1,9 +1,19 @@
 #pragma once
 
 #include "../mcf83xx_common/register_bus.h"
+#include "mcf8316d_registers.h"
 
 namespace mcf8316d_core {
 
 using RegisterBus = mcf83xx_common::RegisterBus;
+using RegisterId = regs::RegisterId;
 
 }  // namespace mcf8316d_core
+
+namespace esphome {
+namespace mcf8316d {
+
+using RegisterId = ::mcf8316d_core::RegisterId;
+
+}  // namespace mcf8316d
+}  // namespace esphome
