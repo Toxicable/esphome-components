06bcbefbf370e0709733d89b10022e05f2f7c061
Fabian Wiles <fabianwiles@canva.com>
2026-07-19 19:35:33 +1000

    Fix MCF8329A typed register namespace

--- components/mcf8329a/mcf8329a_bus.h
+++ components/mcf8329a/mcf8329a_bus.h
@@ -1,9 +1,19 @@
 #pragma once
 
 #include "../mcf83xx_common/register_bus.h"
+#include "mcf8329a_registers.h"
 
 namespace mcf8329a_core {
 
 using RegisterBus = mcf83xx_common::RegisterBus;
+using RegisterId = regs::RegisterId;
 
 }  // namespace mcf8329a_core
+
+namespace esphome {
+namespace mcf8329a {
+
+using RegisterId = ::mcf8329a_core::RegisterId;
+
+}  // namespace mcf8329a
+}  // namespace esphome
